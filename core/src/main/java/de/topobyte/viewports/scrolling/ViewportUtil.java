0aa50d85b551b95194c932322f9e64647b40a769
Sebastian Kürten <sebastian@topobyte.de>
2017-11-16 15:52:52 +0100

    ViewportUtil: add zoomFixed()

--- core/src/main/java/de/topobyte/viewports/scrolling/ViewportUtil.java
+++ core/src/main/java/de/topobyte/viewports/scrolling/ViewportUtil.java
@@ -17,6 +17,8 @@
 
 package de.topobyte.viewports.scrolling;
 
+import de.topobyte.viewports.geometry.Coordinate;
+
 public class ViewportUtil
 {
 
@@ -38,6 +40,33 @@ public class ViewportUtil
 	public static double getViewY(Viewport viewport, double y)
 	{
 		return (y + viewport.getPositionY()) * viewport.getZoom();
+	}
+
+	public static void zoomFixed(Viewport viewport, Coordinate point,
+			boolean in, double zoomStep)
+	{
+		// (x, y) that we want to keep fixed at the screen point (x, y)
+		double frx = getRealX(viewport, point.getX());
+		double fry = getRealY(viewport, point.getY());
+
+		if (in) {
+			viewport.setZoom(viewport.getZoom() * (1 + zoomStep));
+		} else {
+			viewport.setZoom(viewport.getZoom() / (1 + zoomStep));
+		}
+
+		// (x, y) of the (lon, lat) after applying the zoom change
+		double fx = getViewX(viewport, frx);
+		double fy = getViewY(viewport, fry);
+
+		// shift the map to keep the (lon, lat) fixed
+		double dx = fx - point.getX();
+		double dy = fy - point.getY();
+
+		viewport.setPositionX(
+				viewport.getPositionX() - dx / viewport.getZoom());
+		viewport.setPositionY(
+				viewport.getPositionY() - dy / viewport.getZoom());
 	}
 
 }
